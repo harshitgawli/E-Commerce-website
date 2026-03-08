f0bb4a74fb93a5b41957766b3ac0f349a23686c0
harshit gawli <harshitgawli@gmail.com>
2026-03-09 00:17:37 +0530

    admin login page created

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -100,7 +100,23 @@ const registerUser = async(req ,res)=>{
 }
 
 const adminLogin = async(req ,res)=>{
+    try {
+        const {email ,password} = req.body;
+        if(email===process.env.ADMIN_EMAIL && password===process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
 
+            res.json({
+                success:true,
+                message:"Admin login successful",
+                token:token
+            })
+        }
+    } catch (error) {
+        res.json({
+                success:false,
+                message:error.message,
+            })
+    }
 }
 
 
